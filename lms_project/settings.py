1eed57b84c59985087f295577e85d93f7b72068e
Yasser Hegazy <yasseranahegazy@gmail.com>
2026-01-21 12:11:48 +0200

    Register tenant middleware in Django settings

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "lms_project.middleware.TenantMiddleware",
+    "modules.users.middleware.TenantMiddleware",
 ]
 
 ROOT_URLCONF = "lms_project.urls"
